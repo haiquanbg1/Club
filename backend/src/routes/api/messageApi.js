c9db5d40d43670e44bebe8527f68c90d8d007511
Vũ Đức Thắng <125382939+Thangvu21@users.noreply.github.com>
2024-12-07 21:42:14 +0700

    Merge branch 'main' of https://github.com/haiquanbg1/Club

--- backend/src/routes/api/messageApi.js
+++ backend/src/routes/api/messageApi.js
@@ -10,7 +10,6 @@ const router = express.Router();
 router.get("/:conversation_id", authMiddleware, messageController.findAllByConversation);
 router.post("/create", authMiddleware, messageController.create);
 router.delete("/delete", authMiddleware, messageController.drop);
-router.patch("/group/changeReact", authMiddleware, directMessageController.reactChange);
 
 router.post("/:friend_id/send", authMiddleware, directMessageController.create);
 router.delete('/friend/delete/', authMiddleware, directMessageController.drop);
